bb09a6ed75b101d847032eefe5dc5ae0e30e75e5
PJutch <103033969+PJutch@users.noreply.github.com>
2026-04-05 16:20:32 +0300

    Use to_thread to avoid blocking event loop

--- count_words/controller.py
+++ count_words/controller.py
@@ -3,6 +3,7 @@ from fastapi.responses import StreamingResponse
 from count_words.services import count_words
 from count_words.presentation import word_counts_to_xlsx
 import io
+import asyncio
 
 router = APIRouter(prefix="/public/report")
 
@@ -11,9 +12,10 @@ async def test(file: UploadFile = File(...)):
     binary = await file.read()
     text = binary.decode('utf-8')
 
-    word_counts =  count_words(text)
+    word_counts = await asyncio.to_thread(count_words, text)
+    xlsx = await asyncio.to_thread(word_counts_to_xlsx, word_counts)
 
     return StreamingResponse(
-        io.BytesIO(word_counts_to_xlsx(word_counts)),
+        io.BytesIO(xlsx),
         media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
         headers={"Content-Disposition": "attachment; filename=async_export.xlsx"} )
